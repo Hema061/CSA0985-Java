ba405a7236c7c4970e929ccb2420a00aa588f40a
Hema061 <113968433+Hema061@users.noreply.github.com>
2023-07-26 23:03:06 -0400

    Assignment-2

---
+++ factorial.java
@@ -0,0 +1,10 @@
+class FN {
+public static void main(String[] args) {
+        int num = 5;
+        for(int i = 1; i <= num; ++i)
+        {
+        factorial *= i;
+        }
+        System.out.println("Factorial=" + factorial);
+    }
+}
